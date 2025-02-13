383b7459735c25a041f780c203b746aada3271b8
Ezra Hill <ezrahills@gmail.com>
2025-02-13 21:42:48 +0000

    feat: add Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Ensure trailing slashes are used
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
